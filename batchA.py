a8d133b905ed6e4fc993853500122ec9ad86c62b
Jyoshna0126 <126311417+Jyoshna0126@users.noreply.github.com>
2023-02-25 11:36:39 +0530

    Create batchA.py

---
+++ batchA.py
@@ -0,0 +1,7 @@
+x=int(input('enter no of toys:'))
+y=int(input('amount priced for x toys:'))
+z=int(input('amount left:'))
+e=y/z
+t=y+z
+print('each toy price:',e)
+print('initial amount he has:',t)
